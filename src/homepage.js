0479c0ed911d5585724ecb5cd46387762f6617e3
lux-zel <luxvanderzel@outlook.com>
2026-02-06 16:52:01 +0100

    homepage buttons working

---
+++ src/homepage.js
@@ -0,0 +1,18 @@
+import { initializeFirebase } from './firebase-config.js'
+import { signOut } from './auth.js'
+
+initializeFirebase();
+
+const groupsBtn = document.getElementById('groupsBtn');
+const stopwatchBtn = document.getElementById('stopwatchBtn');
+const logoutBtn = document.getElementById('logoutBtn');
+
+groupsBtn.addEventListener('click', () => {
+    window.location.href = './groups.html';
+});
+
+stopwatchBtn.addEventListener('click', () => {
+    window.location.href = './public/stopwatch.html';
+});
+
+logoutBtn.addEventListener('click', signOut);
